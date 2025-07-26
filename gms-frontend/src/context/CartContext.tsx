38ea71a199bcaa31b5fd978d9de730085bf891a1
Deepak Heman Das <deepak.heman-das@familienservice.de>
2025-07-26 15:22:56 +0200

    removed unused import

--- gms-frontend/src/context/CartContext.tsx
+++ gms-frontend/src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, ReactNode } from 'react';
 import usePersistedState from '../hooks/usePersistedState';
 
 interface CartContextType {
